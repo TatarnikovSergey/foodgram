50e5c034c7376b9630688ddb1615933014e21c59
TatarnikovSergey <sg.tatarnikov@yandex.ru>
2024-08-27 23:17:22 +0500

    начинаем рецепты

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,2 +1,11 @@
+from django.contrib.auth import get_user_model
+from django.db import models
 
+User = get_user_model()
+
+
+class Recipies(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL,
+                               related_name="recipies",
+                               verbose_name='Автор')
 
