c469df95f67cc0b6bb5bbae00814aeb7e9fc3442
TatarnikovSergey <sg.tatarnikov@yandex.ru>
2024-09-02 23:03:10 +0500

    пробуем импорт ингредиентов

---
+++ backend/recipes/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import  serializers
+
+from .models import Ingredients, Recipies, Tags
+from users.serialisers import Base64ImageField, UsersSerializer
+
+
+
+class IngredientsSerializer(serializers.ModelSerializer):
+    """Сериализатор ингредиентов."""
+
+    class Meta:
+        model = Ingredients
+        fields = ('id', 'name', 'measurement_unit')
+
+
+class TagsSerializer(serializers.ModelSerializer):
+    """Сериализатор тегов."""
+
+    class Meta:
+        model = Tags
+        fields = ('id', 'name', 'slug')
+
+
+class RecipesSerializer(serializers.ModelSerializer):
+    """Сериализатор рецептов."""
+    tags = TagsSerializer(many=True, read_only=True)
+    author = UsersSerializer(read_only=True)
+    ingredients = IngredientsSerializer(many=True, read_only=True)
+    is_favorited = serializers.SerializerMethodField()
+    is_in_shopping_cart = serializers.SerializerMethodField()
+    image = Base64ImageField()
+
+    class Meta:
+        model = Recipies
+        fields = ('id', 'tags', 'author', 'ingredients', 'is_favorited',
+                  'is_in_shopping_cart', 'name', 'image', 'text',
+                  'cooking_time')
