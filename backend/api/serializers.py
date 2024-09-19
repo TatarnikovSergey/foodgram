68fb866891b1fceaa6be49168fbe3a4a12b273d0
TatarnikovSergey <sg.tatarnikov@yandex.ru>
2024-09-19 20:16:31 +0500

    правка замечаний ревью

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -199,9 +199,9 @@ class RecipiesSerializer(serializers.ModelSerializer):
                         id=ingredient['id']).exists():
                     raise serializers.ValidationError({
                         f'{field}': 'Такого ингредиента не существует!'})
-                if ingredient['amount'] <= 0:
+                if int(ingredient['amount']) <= 0:
                     raise serializers.ValidationError({
-                        'amount': 'Количество не может быть < или = ноль!'})
+                        f'{field}': 'Количество не может быть < или = ноль!'})
         if field == 'tags':
             tags_list = []
             for tag in data:
