7950c53acc7872069579347b0e82853623d6d0d2
TatarnikovSergey <sg.tatarnikov@yandex.ru>
2024-09-20 18:29:14 +0500

    замечания после 2-го ревью

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,7 +8,7 @@ from django.core.files.base import ContentFile
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
-from rest_framework import viewsets, permissions, status, pagination
+from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 import pyshorteners
